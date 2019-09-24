c96aa667af9875bdb093d5042a02cb8a64394847
Daniil <daniel217032001@gmail.com>
2019-09-24 09:21:39 +0300

    add header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,16 @@
-<header>
-
+<header class="header">
+    <div class="header-container">
+        <div class="header-row">
+            <div class="header-logo">
+                <a href="{!! esc_url(home_url( '/' )); !!}">
+                    <img src="{!! get_template_directory_uri() !!}/assets/images/Logo_white.svg" alt="Logo">
+                </a>
+            </div>
+            <div class="header-languageSwitcher"></div>
+            <div class="header-menu"></div>
+            <div class="header-phone">
+                <a href="tel:%%PHONE%%">+7 (958) 756-85-02</a>
+            </div>
+        </div>
+    </div>
 </header>
