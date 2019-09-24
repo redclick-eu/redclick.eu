b3245a7e93ecde653dfe525f7221578242dafcf0
Daniil <daniel217032001@gmail.com>
2019-09-24 22:23:13 +0300

    add services block

--- resources/views/partials/services.blade.php
+++ resources/views/partials/services.blade.php
@@ -1,4 +1,60 @@
 @include("partials.title", ["text" => "Services"])
 <div class="services">
-
+    <div class="services-row">
+        <div class="services-col">
+            <div class="services-card services-card_design">
+                <h4 class="services-title">Design</h4>
+                <ul class="services-list">
+                    <li class="services-item"><a href="#" class="services-link">lorem</a></li>
+                    <li class="services-item"><a href="#" class="services-link">lorem</a></li>
+                    <li class="services-item"><a href="#" class="services-link">lorem</a></li>
+                    <li class="services-item"><a href="#" class="services-link">lorem</a></li>
+                    <li class="services-item"><a href="#" class="services-link">lorem</a></li>
+                    <li class="services-item"><a href="#" class="services-link">lorem</a></li>
+                    <li class="services-item"><a href="#" class="services-link">lorem</a></li>
+                    <li class="services-item"><a href="#" class="services-link">lorem 2lorem2</a></li>
+                    <li class="services-item"><a href="#" class="services-link">lorem</a></li>
+                    <li class="services-item"><a href="#" class="services-link">lorem</a></li>
+                    <li class="services-item"><a href="#" class="services-link">lorem</a></li>
+                    <li class="services-item"><a href="#" class="services-link">lorem</a></li>
+                    <li class="services-item"><a href="#" class="services-link">lorem</a></li>
+                    <li class="services-item"><a href="#" class="services-link">lorem</a></li>
+                    <li class="services-item"><a href="#" class="services-link">lorem</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="services-col">
+            <div class="services-card services-card_development">
+                <h4 class="services-title">Development</h4>
+                <ul class="services-list">
+                    <li class="services-item"><a href="#" class="services-link">lorem</a></li>
+                    <li class="services-item"><a href="#" class="services-link">lorem</a></li>
+                    <li class="services-item"><a href="#" class="services-link">lorem</a></li>
+                    <li class="services-item"><a href="#" class="services-link">lorem</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="services-col">
+            <div class="services-card services-card_promotion">
+                <h4 class="services-title">Promotion</h4>
+                <ul class="services-list">
+                    <li class="services-item"><a href="#" class="services-link">lorem</a></li>
+                    <li class="services-item"><a href="#" class="services-link">lorem</a></li>
+                    <li class="services-item"><a href="#" class="services-link">lorem</a></li>
+                    <li class="services-item"><a href="#" class="services-link">lorem</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="services-col">
+            <div class="services-card services-card_support">
+                <h4 class="services-title">Support</h4>
+                <ul class="services-list">
+                    <li class="services-item"><a href="#" class="services-link">lorem</a></li>
+                    <li class="services-item"><a href="#" class="services-link">lorem</a></li>
+                    <li class="services-item"><a href="#" class="services-link">lorem</a></li>
+                    <li class="services-item"><a href="#" class="services-link">lorem</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </div>
