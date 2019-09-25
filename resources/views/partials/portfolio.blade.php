c472f9d1ab4439a3e64dcb798e37f7787ae7763c
Daniil <daniel217032001@gmail.com>
2019-09-25 18:35:40 +0300

    add portfolio page

--- resources/views/partials/portfolio.blade.php
+++ resources/views/partials/portfolio.blade.php
@@ -1,6 +1,19 @@
 @include("partials.title", ["text" => "Portfolio"])
 <div class="portfolio">
     <div class="portfolio-row">
-
+        <ul class="portfolio-menu">
+            <li class="portfolio-menuItem is-active">lorem</li>
+            <li class="portfolio-menuItem">lorem</li>
+            <li class="portfolio-menuItem">lorem</li>
+            <li class="portfolio-menuItem">lorem</li>
+        </ul>
+        <ul class="portfolio-grid">
+            <li class="portfolio-item"><a href="#" class="portfolio-link" style="background-image: url('http://via.placeholder.com/230x230/FF0000?text=<>')"><span>Lorem ipsum.</span></a></li>
+            <li class="portfolio-item"><a href="#" class="portfolio-link" style="background-image: url('http://via.placeholder.com/230x230/FF0000?text=<>')"><span>Lorem ipsum.</span></a></li>
+            <li class="portfolio-item"><a href="#" class="portfolio-link" style="background-image: url('http://via.placeholder.com/230x230/FF0000?text=<>')"><span>Lorem ipsum.</span></a></li>
+            <li class="portfolio-item"><a href="#" class="portfolio-link" style="background-image: url('http://via.placeholder.com/230x230/FF0000?text=<>')"><span>Lorem ipsum.</span></a></li>
+            <li class="portfolio-item"><a href="#" class="portfolio-link" style="background-image: url('http://via.placeholder.com/230x230/FF0000?text=<>')"><span>Lorem ipsum.</span></a></li>
+            <li class="portfolio-item"><a href="#" class="portfolio-link" style="background-image: url('http://via.placeholder.com/230x230/FF0000?text=<>')"><span>Lorem ipsum.</span></a></li>
+        </ul>
     </div>
 </div>
