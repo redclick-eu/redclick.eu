d0c0c4b9ed15d29bd1c408599868c85cd9d5f640
Daniil <daniel217032001@gmail.com>
2019-10-15 22:31:20 +0300

    update form

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,8 +1,79 @@
+import {validate, validateAll, serialize, reset} from '../util/form'
+import 'whatwg-fetch'
+
 export default {
-  init() {
-    // JavaScript to be fired on all pages
-  },
-  finalize() {
-    // JavaScript to be fired on all pages, after page specific JS is fired
-  },
+    init() {
+
+        const script_recaptcha = document.createElement('script');
+        script_recaptcha.src = 'https://www.google.com/recaptcha/api.js?onload=recaptcha_onload&render=explicit';
+        document.head.appendChild(script_recaptcha);
+
+        window.recaptcha_onload = function () {
+            Array.from(document.getElementsByClassName('g-recaptcha')).forEach(function (_r) {
+                window.grecaptcha.render(_r, {
+                    'sitekey': '6LcFh7AUAAAAAHZr62yo5ptkHQgSo3o_pL2kV4_y',
+                    'callback': function (token) {
+                        _r.setAttribute('data-token', token);
+                        console.log(_r.closest('.js-form-contacts'));
+                        _r.closest('.js-form-contacts').dispatchEvent(new Event('submit'));
+                    },
+                });
+                _r.setAttribute('disabled', 'disabled');
+            });
+        };
+
+        const _forms = document.querySelectorAll('form[data-ajax]');
+        _forms.forEach(_form => {
+            _form.setAttribute('novalidate', '');
+            _form.querySelectorAll('input,textarea,select').forEach(_el => {
+                _el.addEventListener('input', function () {
+                    console.log(this);
+                    validate(this);
+                    validateAll(_form, false);
+                });
+            });
+
+            let sending = false;
+            const _backdrop = _form.querySelector('.js-backdrop');
+            _form.addEventListener('submit', function (e) {
+                e.preventDefault();
+
+                console.log(serialize(_form));
+
+                if (sending || !validateAll(_form)) {
+                    return;
+                }
+
+                sending = true;
+                _form.classList.add('is-loading');
+
+                fetch(_form.getAttribute('data-ajax'), {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: serialize(_form),
+                })
+                    .then(r => r.json())
+                    .catch(err => {
+                        _form.classList.remove('is-loading');
+                        sending = false;
+                        console.log(err);
+                    })
+                    .then((json) => {
+                        console.log(json);
+                        _form.classList.remove('is-loading');
+                        sending = false;
+
+                        if (json.success) {
+                            reset(_form);
+                            _backdrop.classList.add('is-active');
+                            setTimeout(() => _backdrop.classList.remove('is-active'), 3000);
+                        } else if (json.error) {
+                            _form.querySelectorAll('[name="' + json.error.join('"],[name="') + '"]').forEach(el => el.classList.add('is-error'))
+                        }
+                    })
+            });
+        })
+    },
 };
--- resources/views/partials/form-contacts.blade.php
+++ resources/views/partials/form-contacts.blade.php
@@ -1,17 +1,17 @@
-<form class="form_contacts">
+<form class="form_contacts js-form-contacts" action="/" data-ajax="{{get_rest_url( 0, '/redclick/v1/callback' )}}">
     <h2 class="form-header">Write to us:</h2>
     <input type="hidden" value="{!! is_404() ? get_home_url()."/404":get_permalink() !!}" name="pageUrl">
-    <input type="hidden" value="form" name="use">
-    <input type="text" class="form-text form-input" name="name" placeholder="Name">
-    <input type="email" class="form-text form-input" name="mail" placeholder="E-mail" pattern="^[-._a-z0-9]+@(?:[a-z0-9][-a-z0-9]+\.)+[a-z]{2,6}$">
-    <input type="tel" class="form-text form-input" name="phone" placeholder="Phone" pattern="^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$">
-    <input type="text" class="form-text form-input" name="text" placeholder="Message">
+    <input type="text" class="form-text form-input" name="name" placeholder="Name" data-val-type="empty">
+    <input type="email" class="form-text form-input" name="email" placeholder="E-mail" data-val-type="email">
+    <input type="tel" class="form-text form-input" name="phone" placeholder="Phone" data-val-type="phone">
+    <input type="text" class="form-text form-input" name="message" placeholder="Message" data-val-type="10">
     <div class="form-privacyPolicy">
-        <input type="checkbox" id="contactsCheckbox" checked name="info" class="form-checkbox">
+        <input type="checkbox" id="contactsCheckbox" checked name="info" class="form-checkbox" data-val-type="bool">
         <label for="contactsCheckbox"></label>
         <span>I have read and agree to the terms of <span class="form-link" data-toggle="#privacyPolicy, #backdrop">the privacy policy</span>.</span>
     </div>
-    <input type="submit" value="Send" class="form-submit">
+    <input type="submit" value="Send" class="form-submit  g-recaptcha" disabled="disabled">
+    <div class="form-backdrop js-backdrop"><span>Message was successfully delivered</span></div>
 </form>
 
 @include('partials.privacyPolicy')
