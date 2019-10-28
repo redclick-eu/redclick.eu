de2b2387b930c9fff84dfd2572db090af79e4e69
Daniil <daniel217032001@gmail.com>
2019-10-28 22:43:06 +0300

    Update privacyPolicy.blade.php

--- resources/views/partials/privacyPolicy.blade.php
+++ resources/views/partials/privacyPolicy.blade.php
@@ -1,11 +1,8 @@
+@php use App\Controllers\App as A @endphp
 <div class="privacyPolicy" id="privacyPolicy">
     <h4 class="privacyPolicy-header">{!! wpcl_t("Privacy policy") !!}</h4>
     <div class="privacyPolicy-close" data-toggle="#privacyPolicy, #backdrop" data-toggle-type="close"></div>
     <div class="privacyPolicy-content">
-        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, iure obcaecati. Accusantium consectetur debitis, eaque eos ex, expedita facilis fugiat laborum mollitia nam nemo neque odio quas reiciendis totam voluptatem?</div>
-        <div>Doloremque expedita perspiciatis possimus, velit voluptas voluptate. A corporis deserunt ducimus esse fuga incidunt inventore, iure, natus, ratione reiciendis reprehenderit sit suscipit voluptates? Ad aliquid eveniet expedita id placeat quaerat!</div>
-        <div>Deleniti, dolorum eius eum facere fugiat hic nihil obcaecati porro praesentium qui temporibus ullam ut veritatis? Atque consequuntur cupiditate praesentium. Ad commodi consectetur, dolorem excepturi inventore libero non perferendis voluptas?</div>
-        <div>Accusantium, asperiores, beatae cupiditate earum eligendi eos est explicabo fugiat harum incidunt inventore iusto labore, magnam maiores mollitia nemo non obcaecati officiis perferendis quis sed similique soluta temporibus totam ullam.</div>
-        <div>Aliquid aut dicta doloremque ea explicabo facere, impedit minima modi mollitia, necessitatibus nesciunt nobis optio quam ut vel vero voluptatem! Expedita ipsa iste laudantium maiores perspiciatis provident quos ratione voluptatum!</div>
+        {!! A::redclick("PrivacyPolicyWindow") !!}
     </div>
 </div>
