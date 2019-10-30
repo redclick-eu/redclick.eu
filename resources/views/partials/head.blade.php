e93a0580f171ad069e88f4b234ff06d45700cc57
Daniil <daniel217032001@gmail.com>
2019-10-30 18:14:22 +0300

    add favicon

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,8 +3,8 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <link rel="icon" href="@asset('images/favicon.ico')" type="image/x-icon">
-  <link rel="shortcut icon" href="@asset('images/favicon.ico')" type="image/x-icon"/>
+  <link rel="icon" href="{!! App\asset_path("images/favicon.png")  !!}" type="image/x-icon">
+  <link rel="shortcut icon" href="{!! App\asset_path("images/favicon.png")  !!}" type="image/x-icon"/>
 
   <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
   <link href="https://fonts.googleapis.com/css?family=Ubuntu:400,700&display=swap" rel="stylesheet">
