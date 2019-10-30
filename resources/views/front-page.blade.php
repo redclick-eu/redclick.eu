106a844e66169c7476c957765dcc89020306434b
Daniil <daniel217032001@gmail.com>
2019-10-30 18:29:04 +0300

    Update front-page.blade.php

--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
     <main class="main">
-        @include("partials.title", ["text" => "Services"])
+        @include("partials.title", ["text" => wpcl_t("Services")])
         @include("partials.services")
-        @include("partials.title", ["text" => "Portfolio"])
+        @include("partials.title", ["text" => wpcl_t("Portfolio")])
         @include("partials.portfolio")
         @include("partials.reviews")
-        @include("partials.title", ["text" => "Contacts"])
+        @include("partials.title", ["text" => wpcl_t("Contacts")])
         @include("partials.contacts")
     </main>
 @endsection
