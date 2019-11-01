19648815404d5204137744b44e7a92f67724ed7b
Daniil <daniel217032001@gmail.com>
2019-11-01 13:45:47 +0300

    add translation to form

--- resources/views/partials/form-contacts.blade.php
+++ resources/views/partials/form-contacts.blade.php
@@ -1,10 +1,10 @@
 <form class="form_contacts js-form-contacts" action="/" data-ajax="{{get_rest_url( 0, '/redclick/v1/callback' )}}">
     <h2 class="form-header">{!! wpcl_t("Write to us") !!}:</h2>
     <input type="hidden" value="{!! is_404() ? get_home_url()."/404":get_permalink() !!}" name="pageUrl">
-    <input type="text" class="form-text form-input" name="name" placeholder="Name" data-val-type="empty">
+    <input type="text" class="form-text form-input" name="name" placeholder="{!! wpcl_t("Name") !!} data-val-type="empty">
     <input type="email" class="form-text form-input" name="email" placeholder="E-mail" data-val-type="email">
-    <input type="tel" class="form-text form-input" name="phone" placeholder="Phone" data-val-type="phone">
-    <input type="text" class="form-text form-input" name="message" placeholder="Message" data-val-type="10">
+    <input type="tel" class="form-text form-input" name="phone" placeholder="{!! wpcl_t("Phone") !!}" data-val-type="phone">
+    <input type="text" class="form-text form-input" name="message" placeholder="{!! wpcl_t("Message") !!}" data-val-type="10">
     <div class="form-privacyPolicy">
         <input type="checkbox" id="contactsCheckbox" checked name="info" class="form-checkbox" data-val-type="bool">
         <label for="contactsCheckbox"></label>
