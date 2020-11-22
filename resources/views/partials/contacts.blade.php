f26a6971dd6e180ad34481850d65d5dda5d081f0
Daniil <daniel217032001@gmail.com>
2020-11-22 22:02:43 +0300

    https://app.asana.com/0/1135699110101547/1199101812072869
    2) add "Write to Telegram"

--- resources/views/partials/contacts.blade.php
+++ resources/views/partials/contacts.blade.php
@@ -4,7 +4,7 @@
         <h2 class="bcontacts-title">{!! wpcl_t('Connect us!') !!}</h2>
         <span class="bcontacts-subTitle">{!! wpcl_t('By messenger') !!}:</span>
         <div class="bcontacts-social bcontacts-social_tg">
-            <a class="bcontacts-socialBg" href="{{A::redclick('tg_url')}}" target="_blank"><span>{!! wpcl_t('Write Telegram') !!}</span></a>
+            <a class="bcontacts-socialBg" href="{{A::redclick('tg_url')}}" target="_blank"><span>{!! wpcl_t('Write to Telegram') !!}</span></a>
         </div>
         <div class="bcontacts-social bcontacts-social_ms">
             <a class="bcontacts-socialBg" href="{{A::redclick('ms_url')}}" target="_blank"><span>{!! wpcl_t('Chat in Messenger') !!}</span></a>
