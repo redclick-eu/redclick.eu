8ee056e1038feef7f51af2a4207f7bafaaf66a5f
v@v-pc <v@v-pc>
2020-11-24 14:07:33 +0200

    Update footer.blade.php

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -6,9 +6,9 @@
                 <?php wp_nav_menu(["theme_location" => "footer_navigation", 'container_class' => 'menu_footer']) ?>
             </div>
             <div class="footer-links">
-                <a target="_blank" rel="nofollow" href="tel:{!! App\create_link_phone(A::redclick("phone_number")) !!}" class="footer-link footer-link_phone">{!! A::redclick("phone_number") !!}</a>
+                <a href="tel:{!! App\create_link_phone(A::redclick("phone_number")) !!}" class="footer-link footer-link_phone">{!! A::redclick("phone_number") !!}</a>
                 <a href="mailto:{!! A::redclick("email_address") !!}" class="footer-link footer-link_mail">{!! A::redclick("email_address") !!}</a>
-                <a href="{!! A::redclick("fb_url") !!}" class="footer-link footer-link_fb"></a>
+                <a target="_blank" rel="nofollow" href="{!! A::redclick("fb_url") !!}" class="footer-link footer-link_fb"></a>
             </div>
         </div>
     </div>
