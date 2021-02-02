06da90a12aebdb57720b03e2e9f8a074e751f2e0
Daniil Dubchenko <daniel217032001@gmail.com>
2021-02-02 14:06:59 +0300

    Update services.blade.php
    https://app.asana.com/0/1135699110101547/1199881130006843

--- resources/views/partials/services.blade.php
+++ resources/views/partials/services.blade.php
@@ -2,7 +2,7 @@
     <div class="services-row">
         <div class="services-col">
             <div class="services-card services-card_design">
-                <h4 class="services-title">{!! wpcl_t("Design") !!}</h4>
+                <h3 class="services-title">{!! wpcl_t("Design") !!}</h3>
                 <ul class="services-list">
                     @foreach($services_block["design"] as $post)
                         <li class="services-item"><a href="{!! $post["link"] !!}" class="services-link">{!! $post["title"] !!}</a></li>
@@ -12,7 +12,7 @@
         </div>
         <div class="services-col">
             <div class="services-card services-card_development">
-                <h4 class="services-title">{!! wpcl_t("Development") !!}</h4>
+                <h3 class="services-title">{!! wpcl_t("Development") !!}</h3>
                 <ul class="services-list">
                     @foreach($services_block["development"] as $post)
                         <li class="services-item"><a href="{!! $post["link"] !!}" class="services-link">{!! $post["title"] !!}</a></li>
@@ -22,7 +22,7 @@
         </div>
         <div class="services-col">
             <div class="services-card services-card_promotion">
-                <h4 class="services-title">{!! wpcl_t("Promotion") !!}</h4>
+                <h3 class="services-title">{!! wpcl_t("Promotion") !!}</h3>
                 <ul class="services-list">
                     @foreach($services_block["promotion"] as $post)
                         <li class="services-item"><a href="{!! $post["link"] !!}" class="services-link">{!! $post["title"] !!}</a></li>
@@ -32,7 +32,7 @@
         </div>
         <div class="services-col">
             <div class="services-card services-card_support">
-                <h4 class="services-title">{!! wpcl_t("Support") !!}</h4>
+                <h3 class="services-title">{!! wpcl_t("Support") !!}</h3>
                 <ul class="services-list">
                     @foreach($services_block["support"] as $post)
                         <li class="services-item"><a href="{!! $post["link"] !!}" class="services-link">{!! $post["title"] !!}</a></li>
