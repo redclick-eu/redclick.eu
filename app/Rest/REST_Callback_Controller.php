c3b1f818b671be292ba08696f9df134592ead68a
Daniil Dubchenko <daniel217032001@gmail.com>
2021-04-26 02:25:46 +0300

    update rest controllers
    https://app.asana.com/0/1135699110101547/1200244126496651

---
+++ app/Rest/REST_Callback_Controller.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Rest;
+
+use function App\recaptcha_verify;
+
+use WP_Query;
+use WP_REST_Controller;
+use WP_REST_Server;
+
+class REST_Callback_Controller extends WP_REST_Controller {
+    public function __construct() {
+        $this->namespace = SITE_REST_NAMESPACE;
+        $this->rest_base = "/callback";
+    }
+
+    public function register_routes() {
+        register_rest_route($this->namespace, $this->rest_base, [
+            "methods" => WP_REST_Server::CREATABLE,
+            "callback" => [$this, "create_item"],
+            "permission_callback" => [$this, "create_item_permissions_check"],
+            "args" => [
+                "name" => [
+                    "description" => "customer name",
+                    "type" => "string",
+                    "required" => true,
+                    "validate_callback" => function ($param) {
+                        return !empty($param);
+                    }
+                ],
+                "email" => [
+                    "description" => "customer email",
+                    "type" => "string",
+                    "required" => true,
+                    "validate_callback" => function ($param) {
+                        return !empty($param);
+                    }
+                ],
+                "message" => [
+                    "description" => "customer message",
+                    "type" => "string",
+                    "required" => true,
+                    "validate_callback" => function ($param) {
+                        return !empty($param);
+                    }
+                ],
+                "g-recaptcha-response" => [
+                    "description" => "recaptcha response",
+                    "type" => "string",
+                    "required" => true,
+                    "validate_callback" => function ($param) {
+                        return !empty($param);
+                    }
+                ],
+            ]
+        ]);
+    }
+
+    public function create_item_permissions_check($request) {
+        return recaptcha_verify($request['g-recaptcha-response']);
+    }
+
+    public function create_item($request) {
+        $params = $request->get_params();
+
+        $to = get_field('email_address', (new WP_Query(['pagename' => 'mainpage']))->post->ID);
+        $theme = 'Письмо из формы обратной связи';
+
+        $subject = $theme . ' от ' . $params['name'];
+        $message = '
+        <table>
+          <tr>
+            <td>Theme</td>
+            <td>' . $theme . '</td>
+          </tr>
+          <tr>
+            <td>Name</td>
+            <td>' . $params['name'] . '</td>
+          </tr>
+          <tr>
+            <td>E-mail</td>
+            <td>' . $params['email'] . '</td>
+          </tr>
+          <tr>
+            <td>Message</td>
+            <td>' . $params['message'] . '</td>
+          </tr>
+        </table>
+        ';
+
+        $headers = "Content-type: text/html; charset=utf-8\r\n";
+        $headers .= 'Reply-To: ' . $params['email'] . "\r\n";
+
+        wp_mail($to, $subject, $message, $headers);
+
+        return [
+            "success" => true,
+            "message" => "Сообщение успешно доставлено",
+            "\$to" => $to,
+            "\$subject" => $subject,
+            "\$message" => $message,
+            "\$headers" => $headers,
+        ];
+    }
+}
---
+++ app/Rest/REST_Live_Search_Controller.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Rest;
+
+use WP_Query;
+use WP_REST_Controller;
+use WP_REST_Server;
+use WP_Error;
+
+class REST_Live_Search_Controller extends WP_REST_Controller {
+    public function __construct() {
+        $this->namespace = SITE_REST_NAMESPACE;
+        $this->rest_base = "/live_search";
+    }
+
+    public function register_routes() {
+        register_rest_route($this->namespace, $this->rest_base, [
+            "methods" => WP_REST_Server::READABLE,
+            "callback" => [$this, "get_items"],
+            "permission_callback" => "__return_true",
+            "args" => [
+                "keyword" => [
+                    "description" => "search keyword",
+                    "type" => "string",
+                    "required" => true,
+                    "validate_callback" => function ($param) {
+                        return !empty($param);
+                    }
+                ],
+                "size" => [
+                    "description" => "maximum number of pages in search results",
+                    "type" => "integer",
+                    "required" => false,
+                    "validate_callback" => function ($param) {
+                        return $param > 0;
+                    }
+                ]
+            ]
+        ]);
+    }
+
+    public function get_items($request) {
+        $out = [];
+        $params = $request->get_params();
+        $max = 6;
+
+        $the_query = new WP_Query(array(
+            's' => esc_attr($params['keyword']),
+            'post_type' => 'any'
+        ));
+
+        while ($the_query->have_posts()) {
+            $the_query->the_post();
+            $max--;
+            $out[get_the_title()] = [
+                "href" => esc_url(get_the_permalink()),
+                "title" => get_the_title()
+            ];
+            if ($max === 0) {
+                break;
+            }
+        }
+        wp_reset_postdata();
+
+        if($max > 0) {
+            $the_query = new WP_Query(array('post_type' => 'post'));
+            while ($the_query->have_posts()) {
+                $the_query->the_post();
+                if(mb_strpos(mb_strtolower(get_field('content')),mb_strtolower($params['keyword'])) && !isset($out[get_the_title()])) {
+                    $max--;
+                    $out[get_the_title()] = [
+                        "href" => esc_url(get_the_permalink()),
+                        "title" => get_the_title(),
+                    ];
+                }
+                if($max === 0) {
+                    break;
+                }
+            }
+        }
+        wp_reset_postdata();
+
+        $out = array_values($out);
+
+        if($max > 0) {
+            foreach (get_categories() as $cat) {
+                if (strpos(mb_strtolower($cat->cat_name), mb_strtolower($params['keyword'])) !== false) {
+                    $max--;
+                    $out[] = [
+                        "href" => esc_url(get_term_link($cat->term_id)),
+                        "title" => $cat->name
+                    ];
+                }
+                if ($max === 0) break;
+            }
+        }
+
+        if(empty($out)) {
+            $out[] = [
+                "error" => wpcl_t("Sorry, no results were found for your request.")
+            ];
+        }
+
+        return $out;
+    }
+}
