af7a5ace95e43b70608882d279cab6fe0d2855a2
Daniil Dubchenko <daniel217032001@gmail.com>
2021-04-26 00:07:50 +0300

    Update portfolio-grid.js
    https://app.asana.com/0/1135699110101547/1200243808408019

--- resources/assets/scripts/autoload/portfolio-grid.js
+++ resources/assets/scripts/autoload/portfolio-grid.js
@@ -1,20 +1,20 @@
 import Isotope from 'isotope-layout';
 
-const _portfolio = document.querySelector('.js-portfolio');
-if (_portfolio !== null) {
-    const _menuItems = _portfolio.querySelectorAll('.js-portfolio-menu li');
-    const _grid = _portfolio.querySelector('.js-portfolio-grid');
+const portfolio = document.querySelector('.js-portfolio');
+if (portfolio !== null) {
+    const menuItems = portfolio.querySelectorAll('.js-portfolio-menu li');
+    const grid = portfolio.querySelector('.js-portfolio-grid');
 
-    const isotope = new Isotope(_grid, {
+    const isotope = new Isotope(grid, {
         masonry: {
             isFitWidth: true,
             gutter: 20,
         },
     });
 
-    _menuItems.forEach((el) => {
-        el.addEventListener('click', function () {
-            _menuItems.forEach((el) => el.classList.remove('is-active'));
+    menuItems.forEach((el) => {
+        el.addEventListener('click', function callback() {
+            menuItems.forEach((item) => item.classList.remove('is-active'));
             this.classList.add('is-active');
 
             const target = this.getAttribute('data-grid');
