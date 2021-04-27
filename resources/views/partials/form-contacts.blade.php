633cb8e40e76f7f1e2ea5c3727e600b8adfc6f55
Daniil Dubchenko <daniel217032001@gmail.com>
2021-04-28 00:08:12 +0300

    update .form_contacts
    https://app.asana.com/0/1135699110101547/1200243808408009

--- resources/views/partials/form-contacts.blade.php
+++ resources/views/partials/form-contacts.blade.php
@@ -1,22 +1,63 @@
-<form class="form js-form-contacts js-form-validate" id="Form"
+<form class="form form_contacts js-form-validate"
       data-action="{{get_rest_url( 0, '/site/v1/callback' )}}"
       data-settings='@json(['headers' => ['Accept' => 'application/json', 'Content-Type' => 'application/json']])'>
-    <input type="hidden" value="{!! is_404() ? get_home_url()."/404":get_permalink() !!}" name="pageUrl">
+
+    <input type="hidden" value="{!! is_404() ? get_home_url() . "/404" : get_permalink() !!}" name="pageUrl">
+
     <div class="form-block">
-        <input type="text" class="form-input" name="name" placeholder="{!! wpcl_t('Name') !!}" data-val-type="empty">
+        @include('components.input.text', [
+            'placeholder' => wpcl_t('Name'),
+            'name' => 'name',
+            'type' => 'text',
+            'checkType' => 'empty'
+        ])
     </div>
+
     <div class="form-block">
-        <input type="email" class="form-input" name="email" placeholder="E-mail"  data-val-type="email">
+        @include('components.input.text', [
+            'placeholder' => 'E-mail',
+            'name' => 'email',
+            'type' => 'email',
+            'checkType' => 'email'
+        ])
     </div>
+
     <div class="form-block">
-        <input type="tel" class="form-input" name="phone" placeholder="{!! wpcl_t('Phone') !!}" data-val-type="phone">
+        @include('components.input.text', [
+            'placeholder' => wpcl_t('Phone'),
+            'name' => 'phone',
+            'type' => 'tel',
+            'checkType' => 'phone'
+        ])
     </div>
-    <div class="form-block form-block_full  form-block_mm">
-        <textarea name="message" class="form-input form-input_textarea" placeholder="{!! wpcl_t('Text something...') !!}" data-val-type="10"></textarea>
+
+    <div class="form-block form-block_full form-block_mm">
+        @include('components.input.text', [
+            'placeholder' => wpcl_t('Text something...'),
+            'name' => 'message',
+            'type' => 'textarea',
+            'checkType' => '10'
+        ])
     </div>
-    <div class="form-block form-block_full form-block_nm">
-        <div class="form-checkbox"><input disabled type="checkbox" id="contactsCheckbox" checked name="info" class="form-checkboxInput"><label for="contactsCheckbox"></label>{!! wpcl_t('I read and agree with') !!} <span class="form-privacyPolicy" data-toggle="#privacyPolicy, #backdrop" data-target="#PrivacyPolicyWindow">{!! wpcl_t('privacy policy') !!}</span>.</div>
+
+    <div class="form-block form-block_full form-block_nm form-block_inline">
+            @include("components.input.checkbox", [
+                'name' => 'info',
+                'text' => '',
+                'value' => 'true',
+                'is_checked' => true,
+                'is_disabled' => true,
+            ])
+
+            <span>
+                {!! wpcl_t('I read and agree with') !!}
+                <a class="is-link" href="#" data-toggle="#privacyPolicy, #backdrop" data-target="#PrivacyPolicyWindow">{!! wpcl_t('privacy policy') !!}</a>.
+            </span>
     </div>
-    <div class="form-block form-block_full"><input type="submit" value="{!! wpcl_t("Send") !!}" class="form-submit g-recaptcha"></div>
-    <div class="form-backdrop js-backdrop"><span>{!! wpcl_t("Message was successfully delivered") !!}</span></div>
+
+    <div class="form-block form-block_full">
+        <input type="submit" value="{!! wpcl_t("Send") !!}" class="form-submit g-recaptcha">
+    </div>
+
+    <div class="form-backdrop js-backdrop"></div>
 </form>
