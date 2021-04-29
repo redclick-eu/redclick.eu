4ee399d1932b13f87aca39f414c65be463aac0b5
Daniil Dubchenko <daniel217032001@gmail.com>
2021-04-29 19:04:13 +0300

    add .carousel_reviews (replacement for .reviews)

---
+++ resources/views/blocks/carousel/reviews.blade.php
@@ -0,0 +1,20 @@
+<div class="carousel carousel_reviews siema-carousel">
+    <div class="carousel-carousel siema-inner">
+        @foreach($reviews_block as $review)
+            <div class="carousel-item">
+                <div class="carousel-photo">
+                    <img src="{!! $review['photo']['url'] !!}" alt="{!! $review['photo']['alt'] !!}">
+                </div>
+                <div class="carousel-texts">
+                    <p class="carousel-text">{!! $review['text'] !!}<a href="{!! $review['link'] !!}#review" class="carousel-linkToFull">...</a>
+                    </p>
+                    <div class="carousel-name">{!! $review['client_name'] !!}, 
+                        <a href="{!! $review['link'] !!}">{!! $review['company_name'] !!}</a>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+    <div class="carousel-arrow carousel-arrow_left siema-arrow_left"></div>
+    <div class="carousel-arrow carousel-arrow_right siema-arrow_right"></div>
+</div>
--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -7,7 +7,7 @@
         @include("components.title", ["text" => wpcl_t("Portfolio"), "size" => 2])
         @include("blocks.portfolio", ["more_btn" => true])
         @include("components.title", ["text" => wpcl_t("Reviews"), "size" => 2])
-        @include("blocks.reviews")
+        @include("blocks.carousel.reviews")
         @include("components.title", ["text" => wpcl_t("Contacts"), "size" => 2])
         @include("blocks.contacts")
         @include("blocks.callback")
