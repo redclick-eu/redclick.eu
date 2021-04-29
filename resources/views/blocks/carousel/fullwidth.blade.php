2c9b28b9dd97c6af28bc3d68fdd13a1e6f3b68da
Daniil Dubchenko <daniel217032001@gmail.com>
2021-04-29 23:33:18 +0300

    update .carousel_fullwidth

--- resources/views/blocks/carousel/fullwidth.blade.php
+++ resources/views/blocks/carousel/fullwidth.blade.php
@@ -1,11 +1,15 @@
-<div class="carousel carousel_fullwidth @if($separate_arrows) carousel_separateArrows @endif siema-carousel"
+<div class="carousel carousel_fullwidth @if(isset($separate_arrows) && $separate_arrows) carousel_separateArrows @endif siema-carousel"
      data-settings='{"intervalMilliseconds": 5000}'>
     <div class="carousel-inner siema-inner">
         @foreach($images as $image)
             <img src="{!! $image['url'] !!}" alt="{!! $image['alt'] !!}">
         @endforeach
     </div>
-    <div class="carousel-dots siema-dots"></div>
+
+    @if(!isset($separate_arrows) || !$separate_arrows)
+        <div class="carousel-dots siema-dots"></div>
+    @endif
+
     <button class="carousel-arrow carousel-arrow_left siema-arrow_left" aria-label="left"></button>
     <button class="carousel-arrow carousel-arrow_right siema-arrow_right" aria-label="right"></button>
 </div>
