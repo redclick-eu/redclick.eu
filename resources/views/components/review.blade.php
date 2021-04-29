cebd9c19f427f6aab4722044a7a9548351b57d99
Daniil Dubchenko <daniel217032001@gmail.com>
2021-04-30 00:07:39 +0300

    update .review

--- resources/views/components/review.blade.php
+++ resources/views/components/review.blade.php
@@ -1,13 +1,15 @@
-<div class="review" id="review">
-    <div class="review-container">
-        <div class="review-row">
-            <div class="review-image">
-                <img src="{!! $post_vars['review']['photo']['url'] !!}" alt="{!! $post_vars['review']['photo']['alt'] !!}">
-            </div>
-            <div class="review-text">
-                {!! $post_vars['review']['text'] !!}
-                <span>{!! $post_vars['review']['name'] !!}</span>
-            </div>
+@include("components.title", ["text" => $post_vars['title']])
+
+<div class="review">
+    <div class="review-photo">
+        <img src="{!! $post_vars['review']['photo']['url'] !!}" alt="{!! $post_vars['review']['photo']['alt'] !!}">
+    </div>
+    <div class="review-texts">
+        <div class="review-text">
+            {!! $post_vars['review']['text'] !!}
+        </div>
+        <div class="review-name">
+            <span>{!! $post_vars['review']['name'] !!}</span>
         </div>
     </div>
 </div>
