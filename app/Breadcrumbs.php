464016da6f657da619ba4d0959afe5fdf111fa22
Daniil Dubchenko <daniel217032001@gmail.com>
2021-05-01 01:16:04 +0300

    Update Breadcrumbs.php

--- app/Breadcrumbs.php
+++ app/Breadcrumbs.php
@@ -6,27 +6,18 @@ class Breadcrumbs {
 
     public $arg;
 
-    // Локализация
     static $l10n = [];
 
-    // Параметры по умолчанию
     static $args = array(
-        'on_front_page'   => true,  // выводить крошки на главной странице
-        'show_post_title' => true,  // показывать ли название записи в конце (последний элемент). Для записей, страниц, вложений
-        'show_term_title' => true,  // показывать ли название элемента таксономии в конце (последний элемент). Для меток, рубрик и других такс
-        'title_patt'      => '<span class="breadcrumbs-title">%s<meta itemprop="position" content="$$pos$$" /></span>', // шаблон для последнего заголовка. Если включено: show_post_title или show_term_title
-        'last_sep'        => true,  // показывать последний разделитель, когда заголовок в конце не отображается
-        'markup'          => 'schema.org', // 'markup' - микроразметка. Может быть: 'rdf.data-vocabulary.org', 'schema.org', '' - без микроразметки
-        // или можно указать свой массив разметки:
-        // array( 'wrappatt'=>'<div class="breadcrumbs">%s</div>', 'linkpatt'=>'<a href="%s">%s</a>', 'sep_after'=>'', )
-        'priority_tax'    => array('category'), // приоритетные таксономии, нужно когда запись в нескольких таксах
-        'priority_terms'  => array(), // 'priority_terms' - приоритетные элементы таксономий, когда запись находится в нескольких элементах одной таксы одновременно.
-        // Например: array( 'category'=>array(45,'term_name'), 'tax_name'=>array(1,2,'name') )
-        // 'category' - такса для которой указываются приор. элементы: 45 - ID термина и 'term_name' - ярлык.
-        // порядок 45 и 'term_name' имеет значение: чем раньше тем важнее. Все указанные термины важнее неуказанных...
-        'nofollow' => false, // добавлять rel=nofollow к ссылкам?
-
-        // служебные
+        'on_front_page'   => true,
+        'show_post_title' => true,
+        'show_term_title' => true,
+        'title_patt'      => '<span class="breadcrumbs-title">%s<meta itemprop="position" content="$$pos$$" /></span>',
+        'last_sep'        => true,
+        'markup'          => 'schema.org',
+        'priority_tax'    => array('category'),
+        'priority_terms'  => array(),
+        'nofollow' => false,
         'sep'             => '',
         'linkpatt'        => '',
         'pg_end'          => '',
@@ -45,8 +36,6 @@ class Breadcrumbs {
             'attachment' => 'Media: %s',
             'tag'        => 'Tag\'s posts: <b>%s</b>',
             'tax_tag'    => '%1$s from "%2$s" by tag: <b>%3$s</b>',
-            // tax_tag выведет: 'тип_записи из "название_таксы" по тегу: имя_термина'.
-            // Если нужны отдельные холдеры, например только имя термина, пишем так: 'записи по тегу: %3$s'
         );
     }
 
@@ -55,42 +44,36 @@ class Breadcrumbs {
 
         self::$args['sep'] = $sep;
 
-        // Фильтрует дефолты и сливает
         $loc = (object) array_merge( apply_filters('breadcrumbs_default_loc', self::$l10n ), $l10n );
         $arg = (object) array_merge( apply_filters('_breadcrumbs_default_args', self::$args ), $args );
 
-        $arg->sep = '<span class="breadcrumbs-sep">'. $arg->sep .'</span>'; // дополним
+        $arg->sep = '<span class="breadcrumbs-sep">'. $arg->sep .'</span>';
 
-        // упростим
         $sep = & $arg->sep;
         $this->arg = & $arg;
 
-        // микроразметка ---
         if(1){
             $mark = & $arg->markup;
 
-            // Разметка по умолчанию
             if( ! $mark ) $mark = array(
                 'type' =>  'default',
-                'wrappatt'  => '<div class="breadcrumbs">%s</div>',
+                'wrappatt'  => '<div class="breadcrumbs-container">%s</div>',
                 'linkpatt'  => '<a href="%s">%s</a>',
                 'sep_after' => '',
             );
-            // rdf
             elseif( $mark === 'rdf.data-vocabulary.org' ) $mark = array(
                 'type' =>  'rdf.data-vocabulary.org',
-                'wrappatt'   => '<div class="breadcrumbs" prefix="v: http://rdf.data-vocabulary.org/#">%s</div>',
+                'wrappatt'   => '<div class="breadcrumbs-container" prefix="v: http://rdf.data-vocabulary.org/#">%s</div>',
                 'linkpatt'   => '<span typeof="v:Breadcrumb"><a href="%s" rel="v:url" property="v:title">%s</a>',
-                'sep_after'  => '</span>', // закрываем span после разделителя!
+                'sep_after'  => '</span>',
             );
-            // schema.org
+
             elseif( $mark === 'schema.org' ) $mark = array(
                 'type' =>  'schema.org',
-                'wrappatt'   => '<div class="breadcrumbs" itemscope itemtype="http://schema.org/BreadcrumbList">%s</div>',
+                'wrappatt'   => '<div class="breadcrumbs-container" itemscope itemtype="http://schema.org/BreadcrumbList">%s</div>',
                 'linkpatt'   => '<span itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a href="%s" itemprop="item"><span itemprop="name">%s</span></a><meta itemprop="position" content="$$pos$$" /></span>',
                 'sep_after'  => '',
             );
-
             elseif( ! is_array($mark) )
                 die( __CLASS__ .': "markup" parameter must be array...');
 
@@ -99,11 +82,10 @@ class Breadcrumbs {
             $arg->sep      .= $mark['sep_after']."\n";
         }
 
-        $linkpatt = $arg->linkpatt; // упростим
+        $linkpatt = $arg->linkpatt;
 
         $q_obj = get_queried_object();
 
-        // может это архив пустой таксы?
         $ptype = null;
         if( empty($post) ){
             if( isset($q_obj->taxonomy) )
@@ -111,19 +93,17 @@ class Breadcrumbs {
         }
         else $ptype = & $wp_post_types[ $post->post_type ];
 
-        // paged
         $arg->pg_end = '';
         if( ($paged_num = get_query_var('paged')) || ($paged_num = get_query_var('page')) )
             $arg->pg_end = $sep . sprintf( $loc->paged, (int) $paged_num );
 
-        $pg_end = $arg->pg_end; // упростим
+        $pg_end = $arg->pg_end;
 
         $out = '';
 
         if( is_front_page() ){
             return $arg->on_front_page ? sprintf( $wrappatt, ( $paged_num ? sprintf($linkpatt, get_home_url(), $loc->home, 1) . $pg_end : $loc->home ) ) : '';
         }
-        // страница записей, когда для главной установлена отдельная страница.
         elseif( is_home() ) {
             $out = $paged_num ? ( sprintf( $linkpatt, get_permalink($q_obj), esc_html($q_obj->post_title), 1 ) . $pg_end ) : esc_html($q_obj->post_title);
         }
@@ -144,7 +124,6 @@ class Breadcrumbs {
                 $tit = sprintf( $loc->year, $year );
                 $out = ( $paged_num ? sprintf($linkpatt, $y_url, $tit, 1) . $pg_end : $tit );
             }
-            // month day
             else {
                 $y_link = sprintf( $linkpatt, $y_url, $year, 1);
                 $m_url  = get_month_link( $year, get_the_time('m') );
@@ -159,29 +138,22 @@ class Breadcrumbs {
                 }
             }
         }
-        // Древовидные записи
         elseif( is_singular() && $ptype->hierarchical ){
             $out = $this->_add_title( $this->_page_crumbs($post), $post );
         }
-        // Таксы, плоские записи и вложения
         else {
-            $term = $q_obj; // таксономии
+            $term = $q_obj;
 
-            // определяем термин для записей (включая вложения attachments)
             if( is_singular() ){
-                // изменим $post, чтобы определить термин родителя вложения
                 if( is_attachment() && $post->post_parent ){
-                    $save_post = $post; // сохраним
+                    $save_post = $post;
                     $post = get_post($post->post_parent);
                 }
 
-                // учитывает если вложения прикрепляются к таксам древовидным - все бывает :)
                 $taxonomies = get_object_taxonomies( $post->post_type );
-                // оставим только древовидные и публичные, мало ли...
                 $taxonomies = array_intersect( $taxonomies, get_taxonomies( array('hierarchical' => true, 'public' => true) ) );
 
                 if( $taxonomies ){
-                    // сортируем по приоритету
                     if( ! empty($arg->priority_tax) ){
                         usort( $taxonomies, function($a,$b)use($arg){
                             $a_index = array_search($a, $arg->priority_tax);
@@ -190,14 +162,12 @@ class Breadcrumbs {
                             $b_index = array_search($b, $arg->priority_tax);
                             if( $b_index === false ) $b_index = 9999999;
 
-                            return ( $b_index === $a_index ) ? 0 : ( $b_index < $a_index ? 1 : -1 ); // меньше индекс - выше
+                            return ( $b_index === $a_index ) ? 0 : ( $b_index < $a_index ? 1 : -1 );
                         } );
                     }
 
-                    // пробуем получить термины, в порядке приоритета такс
                     foreach( $taxonomies as $taxname ){
                         if( $terms = get_the_terms( $post->ID, $taxname ) ){
-                            // проверим приоритетные термины для таксы
                             $prior_terms = & $arg->priority_terms[ $taxname ];
                             if( $prior_terms && count($terms) > 2 ){
                                 foreach( (array) $prior_terms as $term_id ){
@@ -218,16 +188,12 @@ class Breadcrumbs {
                     }
                 }
 
-                if( isset($save_post) ) $post = $save_post; // вернем обратно (для вложений)
+                if( isset($save_post) ) $post = $save_post;
             }
 
-            // вывод
-
-            // все виды записей с терминами или термины
             if( $term && isset($term->term_id) ){
                 $term = apply_filters('breadcrumbs_term', $term );
 
-                // attachment
                 if( is_attachment() ){
                     if( ! $post->post_parent )
                         $out = sprintf( $loc->attachment, esc_html($post->post_title) );
@@ -240,26 +206,22 @@ class Breadcrumbs {
                         }
                     }
                 }
-                // single
+
                 elseif( is_single() ){
                     if( ! $out = apply_filters('post_tax_crumbs', '', $term, $this ) ){
                         $_crumbs = $this->_tax_crumbs( $term, 'self' );
                         $out = $this->_add_title( $_crumbs, $post );
                     }
                 }
-                // не древовидная такса (метки)
                 elseif( ! is_taxonomy_hierarchical($term->taxonomy) ){
-                    // метка
                     if( is_tag() )
                         $out = $this->_add_title('', $term, sprintf( $loc->tag, esc_html($term->name) ) );
-                    // такса
                     elseif( is_tax() ){
                         $post_label = $ptype->labels->name;
                         $tax_label = $GLOBALS['wp_taxonomies'][ $term->taxonomy ]->labels->name;
                         $out = $this->_add_title('', $term, sprintf( $loc->tax_tag, $post_label, $tax_label, esc_html($term->name) ) );
                     }
                 }
-                // древовидная такса (рибрики)
                 else {
                     if( ! $out = apply_filters('term_tax_crumbs', '', $term, $this ) ){
                         $_crumbs = $this->_tax_crumbs( $term, 'parent' );
@@ -267,13 +229,11 @@ class Breadcrumbs {
                     }
                 }
             }
-            // влоежния от записи без терминов
             elseif( is_attachment() ){
                 $parent = get_post($post->post_parent);
                 $parent_link = sprintf( $linkpatt, get_permalink($parent), esc_html($parent->post_title),1 );
                 $_out = $parent_link;
 
-                // вложение от записи древовидного типа записи
                 if( is_post_type_hierarchical($parent->post_type) ){
                     $parent_crumbs = $this->_page_crumbs($parent);
                     $_out = implode( $sep, array( $parent_crumbs, $parent_link ) );
@@ -281,30 +241,25 @@ class Breadcrumbs {
 
                 $out = $this->_add_title( $_out, $post );
             }
-            // записи без терминов
             elseif( is_singular() ){
                 $out = $this->_add_title( '', $post );
             }
         }
 
-        // замена ссылки на архивную страницу для типа записи
         $home_after = apply_filters('breadcrumbs_home_after', '', $linkpatt, $sep, $ptype );
 
         if( '' === $home_after ){
-            // Ссылка на архивную страницу типа записи для: отдельных страниц этого типа; архивов этого типа; таксономий связанных с этим типом.
             if( $ptype && $ptype->has_archive && ! in_array( $ptype->name, array('post','page','attachment') )
                 && ( is_post_type_archive() || is_singular() || (is_tax() && in_array($term->taxonomy, $ptype->taxonomies)) )
             ){
                 $pt_title = $ptype->labels->name;
 
-                // первая страница архива типа записи
                 if( is_post_type_archive() && ! $paged_num )
                     $home_after = sprintf( $this->arg->title_patt, $pt_title );
-                // singular, paged post_type_archive, tax
                 else{
                     $home_after = sprintf( $linkpatt, get_post_type_archive_link($ptype->name), $pt_title );
 
-                    $home_after .= ( ($paged_num && ! is_tax()) ? $pg_end : $sep ); // пагинация
+                    $home_after .= ( ($paged_num && ! is_tax()) ? $pg_end : $sep );
                 }
             }
         }
@@ -352,25 +307,23 @@ class Breadcrumbs {
         return '';
     }
 
-    // добалвяет заголовок к переданному тексту, с учетом всех опций. Добавляет разделитель в начало, если надо.
     function _add_title( $add_to, $obj, $term_title = '' ){
-        $arg = & $this->arg; // упростим...
-        $title = $term_title ? $term_title : esc_html($obj->post_title); // $term_title чиститься отдельно, теги моугт быть...
+        $arg = & $this->arg;
+        $title = $term_title ? $term_title : esc_html($obj->post_title);
         $show_title = $term_title ? $arg->show_term_title : $arg->show_post_title;
 
-        // пагинация
         if( $arg->pg_end ){
             $link = $term_title ? get_term_link($obj) : get_permalink($obj);
             $add_to .= ($add_to ? $arg->sep : '') . sprintf( $arg->linkpatt, $link, $title ) . $arg->pg_end;
         }
-        // дополняем - ставим sep
+
         elseif( $add_to ){
             if( $show_title )
                 $add_to .= $arg->sep . sprintf( $arg->title_patt, $title );
             elseif( $arg->last_sep )
                 $add_to .= $arg->sep;
         }
-        // sep будет потом...
+
         elseif( $show_title )
             $add_to = sprintf( $arg->title_patt, $title );
 
